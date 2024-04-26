f144e90a340d0daf3e09c997a54c27edf27f7d31
Lucas Plabst <lplabst@colostate.edu>
2024-04-25 20:17:40 -0600

    only need to take screenshots, implementation DONE

--- My project/Assets/GlobalManager.cs
+++ My project/Assets/GlobalManager.cs
@@ -4,30 +4,50 @@ using UnityEngine;
 
 public class GlobalManager : MonoBehaviour
 {
-    [HideInInspector]public GameObject ZPlane;
-    [HideInInspector]public GameObject YPlane;
-    [HideInInspector]public GameObject XPlane;
+    [HideInInspector] public GameObject ZPlane;
+    [HideInInspector] public GameObject YPlane;
+    [HideInInspector] public GameObject XPlane;
     public GameObject ZPos;
     public GameObject YPos;
     public GameObject XPos;
-    [HideInInspector]public GameObject ZCopy;
-    [HideInInspector]public GameObject YCopy;
-    [HideInInspector]public GameObject XCopy;
+    [HideInInspector] public GameObject ZCopy;
+    [HideInInspector] public GameObject YCopy;
+    [HideInInspector] public GameObject XCopy;
+
+    public GameObject[] cameras;
+    List<Vector3> cameraPositions = new List<Vector3>();
     // Start is called before the first frame update
     void Start()
     {
-        
+        StoreCameraPositions();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void flush()
     {
         Destroy(ZCopy);
         Destroy(YCopy);
         Destroy(XCopy);
+        RestoreCameraPositions();
+    }
+    void StoreCameraPositions()
+    {
+
+        foreach (GameObject camera in cameras)
+        {
+            Vector3 position = camera.transform.position;
+            cameraPositions.Add(position);
+        }
+    }
+    void RestoreCameraPositions()
+    {
+        for (int i = 0; i < cameras.Length; i++)
+        {
+            cameras[i].transform.position = cameraPositions[i];
+        }
     }
 }
--- My project/Assets/Manipulation.cs
+++ My project/Assets/Manipulation.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Manipulation : MonoBehaviour
 {
+    public GameObject globalManager;
+    GlobalManager managerScript;
     // Start is called before the first frame update
     void Start()
     {
-
+        GameObject manager = GameObject.FindWithTag("GlobalManager");
+        managerScript = manager.GetComponent<GlobalManager>();
     }
 
     // Update is called once per frame
@@ -17,14 +20,27 @@ public class Manipulation : MonoBehaviour
     }
     public void ScaleUp()
     {
-        if (transform.localScale.x < 1.75f)
-        transform.localScale += new Vector3(0.25f, 0.25f, 0.25f);
+        if (transform.localScale.x <= 1.75f)
+        {
+            transform.localScale += new Vector3(0.25f, 0.25f, 0.25f);
+            foreach (GameObject camera in managerScript.cameras)
+            {
+                camera.transform.position += new Vector3(0,0,-0.1f);
+            }
+        }
     }
+
 
     public void ScaleDown()
     {
         if (transform.localScale.x > 0.25f)
+        {
             transform.localScale += new Vector3(-0.25f, -0.25f, -0.25f);
+            foreach (GameObject camera in managerScript.cameras)
+            {
+                camera.transform.position += new Vector3(0,0,0.1f);
+            }
+        }
     }
 
     public void RotateLeft()
@@ -35,4 +51,9 @@ public class Manipulation : MonoBehaviour
     {
         transform.Rotate(0, 15, 0);
     }
+    public void ResetRotation()
+    {
+         transform.localRotation = Quaternion.identity;
+    }
+
 }
